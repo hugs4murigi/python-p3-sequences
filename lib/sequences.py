621432627d406b0467ef95a8284ae410c467933d
brian <brianmurigi19@gmail.com>
2024-03-27 13:33:53 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    count  = 0
+    sol = []
+
+    while count < length:
+        sol.append(a)
+        c = a + b
+        a = b
+        b = c
+        count += 1
+    print(sol)
+
+print_fibonacci(9)
